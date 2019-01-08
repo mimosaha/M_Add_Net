4d8c19ea3b9fcfb0d51e2fa348c2cfadc132234d
Mimo <mimosaha@gmail.com>
2019-01-08 18:13:57 +0600

    Add RM with project

--- app/src/main/java/portal/remote/superpeer/remote/com/m_add_net/MainActivity.java
+++ app/src/main/java/portal/remote/superpeer/remote/com/m_add_net/MainActivity.java
@@ -2,13 +2,56 @@ package portal.remote.superpeer.remote.com.m_add_net;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import io.left.rightmesh.android.AndroidMeshManager;
+import io.left.rightmesh.id.MeshId;
+import io.left.rightmesh.mesh.MeshStateListener;
+import io.left.rightmesh.util.RightMeshException;
+
+public class MainActivity extends AppCompatActivity implements MeshStateListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Add comment
+
+        configureMesh();
+    }
+
+    private AndroidMeshManager androidMeshManager;
+
+    private void configureMesh() {
+        try {
+            androidMeshManager = AndroidMeshManager
+                    .getInstance(this, this, null, "M_Net_1");
+            androidMeshManager.resume();
+        } catch (RightMeshException.RightMeshServiceDisconnectedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void meshStateChanged(MeshId meshId, int state) {
+        if (state == MeshStateListener.SUCCESS) {
+            try {
+                androidMeshManager.bind(10623);
+                Log.v("MIMO_SAHA: ", "PeerId: " + meshId);
+
+            } catch (RightMeshException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            androidMeshManager.stop();
+        } catch (RightMeshException.RightMeshServiceDisconnectedException e) {
+            e.printStackTrace();
+        }
     }
 }
